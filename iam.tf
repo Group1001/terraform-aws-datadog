d8eae3d51126a237f2d4adecf36646dbe698740d
Lucas do Amaral Saboya <lucas.doamaral-saboya@group1001.com>
2020-06-01 18:16:50 -0300

    Fix s3 bucket setup.

--- iam.tf
+++ iam.tf
@@ -24,5 +24,5 @@ resource "aws_iam_role_policy_attachment" "datadog_core" {
 resource "aws_iam_role_policy_attachment" "datadog_core_logs" {
   count      = var.enable_datadog_aws_integration ? 1 : 0
   role       = aws_iam_role.datadog_integration[0].name
-  policy_arn = aws_iam_policy.datadog_rehidration_core[0].arn
+  policy_arn = data.aws_iam_policy_document.datadog_rehidration_core[0].arn
 }
