908d10b83c46a7b71aa741c308f8f34ddb832494
Lucas do Amaral Saboya <lucas.doamaral-saboya@group1001.com>
2020-06-01 17:23:51 -0300

    Enable log bucket encryption.

--- s3.tf
+++ s3.tf
@@ -5,7 +5,13 @@ resource "aws_s3_bucket" "this" {
     local.default_tags, {
       service = "Datadog"
   })
-
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
   lifecycle_rule {
     enabled = true
     transition {
